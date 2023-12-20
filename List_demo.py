05019cccaa7fe0e3da95673ac6fc886b16e798f7
harshali pawar <harshalipawar25@gmail.com>
2023-12-20 20:17:23 +0530

    add list_demo all list operation

---
+++ List_demo.py
@@ -0,0 +1,65 @@
+# example 1
+lst1 = [1,2,2,3,5,4,6]
+lst2 = ["Red", "Green", "Blue"]
+print(lst1)
+print(lst2)
+# example 2
+details = ["Abhijeet", 18, "FYBScIT", 9.8]
+print(details)
+
+# list index 
+#Example 1
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [0]      [1]     [2]      [3]      [4]
+print(colors[2])
+print(colors[4])
+print(colors[0])
+
+# nagative indexing 
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [-5]    [-4]    [-3]     [-2]      [-1]
+print(colors[-1])
+print(colors[-3])
+print(colors[-5])
+
+#Check whether an item in present in the list?
+
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+if "Yellow" in colors:
+    print("Yellow is present.")
+else:
+    print("Yellow is absent.")
+
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+if "Orange" in colors:
+    print("Orange is present.")
+else:
+    print("Orange is absent.")
+
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[3:7])	#using positive indexes
+print(animals[-7:-2])	#using negative indexes'
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[1:8:3])
+
+# List Comprehension
+# List comprehensions are used for creating new lists from other iterables like lists, tuples, dictionaries, sets, and even in arrays and strings.
+
+# Syntax
+# List = [Expression(item) for item in iterable if Condition]
+# Expression: It is the item which is being iterated.
+# Iterable: It can be list, tuples, dictionaries, sets, and even in arrays and strings.
+# Condition: Condition checks if the item should be added to the new list or not.
+
+# Example 1: 
+
+names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+namesWith_O = [item for item in names if "o" in item]
+print(namesWith_O)
+
+#Example 2: Accepts items which have more than 4 letters
+names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+namesWith_O = [item for item in names if (len(item) > 4)]
+print(namesWith_O)
